4abe59e36bc450de58e3db49c2855718ed13606f
the-code-learner <142033899+the-code-learner@users.noreply.github.com>
2026-08-18 15:15:56 +0200

    fix validator voting power arithmetic helper

--- internal/ledger/arithmetic.go
+++ internal/ledger/arithmetic.go
@@ -1,6 +1,10 @@
 package ledger
 
-import "math"
+import (
+	"math"
+
+	"github.com/zephyr-chain/zephyr-chain/internal/dpos"
+)
 
 func addUint64(left uint64, right uint64) (uint64, bool) {
 	if right > math.MaxUint64-left {
@@ -16,18 +20,14 @@ func nextUint64(value uint64) (uint64, bool) {
 	return value + 1, true
 }
 
-func sumValidatorVotingPower(validators []validatorVotingPowerView) (uint64, bool) {
+func sumValidatorVotingPower(validators []dpos.Validator) (uint64, bool) {
 	var total uint64
 	for _, validator := range validators {
-		next, ok := addUint64(total, validator.votingPower())
+		next, ok := addUint64(total, validator.VotingPower)
 		if !ok {
 			return 0, false
 		}
 		total = next
 	}
 	return total, true
-}
-
-type validatorVotingPowerView interface {
-	votingPower() uint64
 }
