5c16056b26947d9029aa0196df41f0dc38bb4569
the-code-learner <142033899+the-code-learner@users.noreply.github.com>
2026-08-19 18:04:50 +0200

    gofmt token policy parallel tests

--- internal/v2/execution/token_policy_parallel_test.go
+++ internal/v2/execution/token_policy_parallel_test.go
@@ -17,17 +17,17 @@ func TestTokenDefinitionPolicyWitnessIsSharedReadForTransfers(t *testing.T) {
 	coinB[0] = 4
 	definition := object.Object{ID: definitionID, Version: 1, Kind: object.KindTokenDefinition}
 	first := tx.Transaction{
-		StateRoot: root,
-		Inputs: []tx.InputRef{{ObjectID: coinA}, {ObjectID: definitionID}},
+		StateRoot:  root,
+		Inputs:     []tx.InputRef{{ObjectID: coinA}, {ObjectID: definitionID}},
 		Operations: []tx.Operation{{Kind: tx.OpTransfer}},
-		Witnesses: []tx.Witness{{Object: object.Object{ID: coinA, Kind: object.KindCoin}}, {Object: definition}},
+		Witnesses:  []tx.Witness{{Object: object.Object{ID: coinA, Kind: object.KindCoin}}, {Object: definition}},
 	}
 	first.Salt[0] = 1
 	second := tx.Transaction{
-		StateRoot: root,
-		Inputs: []tx.InputRef{{ObjectID: coinB}, {ObjectID: definitionID}},
+		StateRoot:  root,
+		Inputs:     []tx.InputRef{{ObjectID: coinB}, {ObjectID: definitionID}},
 		Operations: []tx.Operation{{Kind: tx.OpTransfer}},
-		Witnesses: []tx.Witness{{Object: object.Object{ID: coinB, Kind: object.KindCoin}}, {Object: definition}},
+		Witnesses:  []tx.Witness{{Object: object.Object{ID: coinB, Kind: object.KindCoin}}, {Object: definition}},
 	}
 	second.Salt[0] = 2
 	if err := validateIndependentBatch([]tx.Transaction{first, second}); err != nil {
@@ -43,17 +43,17 @@ func TestTokenDefinitionWriteConflictsWithTransferRead(t *testing.T) {
 	coinA[0] = 3
 	definition := object.Object{ID: definitionID, Version: 1, Kind: object.KindTokenDefinition}
 	transfer := tx.Transaction{
-		StateRoot: root,
-		Inputs: []tx.InputRef{{ObjectID: coinA}, {ObjectID: definitionID}},
+		StateRoot:  root,
+		Inputs:     []tx.InputRef{{ObjectID: coinA}, {ObjectID: definitionID}},
 		Operations: []tx.Operation{{Kind: tx.OpTransfer}},
-		Witnesses: []tx.Witness{{Object: object.Object{ID: coinA, Kind: object.KindCoin}}, {Object: definition}},
+		Witnesses:  []tx.Witness{{Object: object.Object{ID: coinA, Kind: object.KindCoin}}, {Object: definition}},
 	}
 	transfer.Salt[0] = 1
 	mint := tx.Transaction{
-		StateRoot: root,
-		Inputs: []tx.InputRef{{ObjectID: definitionID}},
+		StateRoot:  root,
+		Inputs:     []tx.InputRef{{ObjectID: definitionID}},
 		Operations: []tx.Operation{{Kind: tx.OpMintToken}},
-		Witnesses: []tx.Witness{{Object: definition}},
+		Witnesses:  []tx.Witness{{Object: definition}},
 	}
 	mint.Salt[0] = 2
 	if err := validateIndependentBatch([]tx.Transaction{transfer, mint}); err != ErrBatchConflict {
